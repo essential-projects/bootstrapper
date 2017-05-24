c0929fa7f25872c2f9e5f9848c99c3ca5c34fbc2
Simon Reymann <simon.reymann@gmail.com>
2017-05-24 16:41:27 +0200

    build dist

--- dist/commonjs/extension_bootstrapper.js
+++ dist/commonjs/extension_bootstrapper.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const utils_1 = require("@process-engine-js/utils");
 const bluebirdPromise = require("bluebird");
 class ExtensionBootstrapper {
