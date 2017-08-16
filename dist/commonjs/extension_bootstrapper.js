809a7cfb181e0124e0950906ab3d459b069f7e35
Heiko Mathes <heiko.mathes@gmail.com>
2017-08-16 19:52:02 +0200

    start lifecycle

--- dist/commonjs/extension_bootstrapper.js
+++ dist/commonjs/extension_bootstrapper.js
@@ -9,23 +9,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const utils_1 = require("@process-engine-js/utils");
-const bluebirdPromise = require("bluebird");
 class ExtensionBootstrapper {
     constructor(_container, _extensionDiscoveryTag) {
         this._extensionInstances = [];
-        this._isInitialized = false;
         this._container = _container;
         this._extensionDiscoveryTag = _extensionDiscoveryTag;
         if (typeof _container === 'undefined') {
             throw new Error('IoC container is required.');
         }
         this._registerInstanceToIocContainer(this);
-    }
-    get isInitialized() {
-        return this._isInitialized;
-    }
-    set isInitialized(initialize) {
-        this._isInitialized = initialize;
     }
     get container() {
         return this._container;
@@ -42,47 +34,37 @@ class ExtensionBootstrapper {
             this.container.registerObject(registrationKey, instance);
         }
     }
-    initialize() {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (!this.isInitialized) {
-                yield this.initializeExtensions();
-                this.isInitialized = true;
-            }
-        });
-    }
     _discoverExtensionKeys(extensionDiscoveryTag) {
         return this.container.getKeysByTags(extensionDiscoveryTag);
     }
-    initializeExtensions() {
-        const discoveredExtensionKeys = this._discoverExtensionKeys(this.extensionDiscoveryTag);
-        const serialPromise = bluebirdPromise.mapSeries(discoveredExtensionKeys, (extensionKey) => {
-            return this.initializeExtension(extensionKey);
-        });
-        return serialPromise;
-    }
-    initializeExtension(extensionKey) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const instance = this.container.resolve(extensionKey);
-            yield utils_1.executeAsExtensionHookAsync(instance.initialize, instance);
-            this[instance.name] = instance;
-            this.extensionInstances.push(instance);
-        });
-    }
     start() {
         return __awaiter(this, void 0, void 0, function* () {
-            yield this.initialize();
+            console.log('-----------------asdasd');
             yield this.startExtensions();
         });
     }
     startExtensions() {
-        const serialPromise = bluebirdPromise.mapSeries(this.extensionInstances, (extensionInstance) => {
-            return this.startExtension(extensionInstance);
+        return __awaiter(this, void 0, void 0, function* () {
+            const extensions = yield this._discoverExtensions();
+            console.log(extensions);
+            return Promise.all(extensions.map((extension) => {
+                return this.startExtension(extension);
+            }));
         });
-        return serialPromise;
     }
     startExtension(instance) {
         return __awaiter(this, void 0, void 0, function* () {
             yield utils_1.executeAsExtensionHookAsync(instance.start, instance);
+        });
+    }
+    _discoverExtensions() {
+        const discoveredExtensionKeys = this._discoverExtensionKeys(this.extensionDiscoveryTag);
+        return Promise.all(discoveredExtensionKeys.map((extensionKey) => {
+            return this.container.resolveAsync(extensionKey);
+        }))
+            .catch((error) => {
+            console.log('discover extensions error', error);
+            throw error;
         });
     }
 }
--- dist/extension_bootstrapper.d.ts
+++ dist/extension_bootstrapper.d.ts
@@ -1,6 +1,6 @@
 import { Container, IInstanceWrapper } from 'addict-ioc';
 export interface IExtension {
-    initialize(): Promise<void>;
+    y: any;
     start(): Promise<void>;
     name?: string;
 }
@@ -8,18 +8,14 @@ export declare class ExtensionBootstrapper {
     private _container;
     private _extensionDiscoveryTag;
     private _extensionInstances;
-    private _isInitialized;
     constructor(_container: Container<IInstanceWrapper<any>>, _extensionDiscoveryTag: string);
-    protected isInitialized: boolean;
     protected readonly container: Container<IInstanceWrapper<any>>;
     readonly extensionDiscoveryTag: string;
     readonly extensionInstances: Array<IExtension>;
     protected _registerInstanceToIocContainer(instance: IExtension): void;
-    initialize(): Promise<void>;
     protected _discoverExtensionKeys(extensionDiscoveryTag: string): Array<string>;
-    protected initializeExtensions(): Promise<any>;
-    protected initializeExtension(extensionKey: string): Promise<void>;
     start(): Promise<void>;
-    protected startExtensions(): Promise<any>;
+    protected startExtensions(): Promise<Array<void>>;
     protected startExtension(instance: IExtension): Promise<void>;
+    private _discoverExtensions();
 }
